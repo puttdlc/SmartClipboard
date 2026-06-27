addcac3fdeacb3ad9ddcce8ac71497d33e40d971
puttdlc <userputt12343@gmail.com>
2026-06-27 11:31:21 +0700

    Initial Commit

---
+++ SmartClipboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartClipboard
+//
+//  Created by Pacharapol Padungkarn on 27/6/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartClipboard/SmartClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartClipboardApp.swift
+//  SmartClipboard
+//
+//  Created by Pacharapol Padungkarn on 27/6/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct SmartClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
